1043c4d40ca2aacf47d1c6e0e28bed85b8c4a140
tomstao <96sutao@gmail.com>
2024-11-07 12:51:31 -0500

    pyramid

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int rows;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for generating pyramid: ");
+        rows = scanner.nextInt();
+        for (int i = 1 ; i <= rows ; i++) {
+
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+
+    }
+}
